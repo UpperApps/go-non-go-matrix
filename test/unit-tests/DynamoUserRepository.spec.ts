275cb53e398699b20eef77cc777990b721453b3c
Rodrigo Melo <upperapps@gmail.com>
2024-01-06 21:45:23 -0700

    Implemented user repository
     Author:    Rodrigo Melo <upperapps@gmail.com>

---
+++ test/unit-tests/DynamoUserRepository.spec.ts
@@ -0,0 +1,114 @@
+import 'reflect-metadata';
+import { v4 as uuidv4 } from 'uuid';
+import { fakerEN as faker } from '@faker-js/faker';
+import { User } from '../../src/domain/user/User';
+import { UserRepository } from '../../src/domain/user/UserRepository';
+import { Test, TestingModule } from '@nestjs/testing';
+import DynamoUserRepository from '../../src/infrastructure/DynamoUserRepository';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+import DynamoDBConfig from '../../src/infrastructure/config/DynamoDBConfig';
+
+let user: User;
+let userRepository: UserRepository;
+
+beforeEach(async () => {
+  const testingModule: TestingModule = await Test.createTestingModule({
+    providers: [
+      {
+        provide: DynamoDBDocument,
+        useValue: DynamoDBConfig.getDynamoDBDocument(),
+      },
+      {
+        provide: UserRepository,
+        useClass: DynamoUserRepository,
+      }],
+  }).compile();
+
+  userRepository = testingModule.get<UserRepository>(UserRepository);
+
+  user = {
+    id: uuidv4(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    createdAt: new Date(),
+  };
+});
+
+afterEach(async () => {
+  const users = await userRepository.findAll();
+
+  for (const user of users) {
+    await userRepository.delete(user.id);
+  }
+});
+
+describe('Test User DyanamoDB repository', () => {
+  it('should save a user and find it by id', async () => {
+    const user: User = {
+      id: uuidv4(),
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      createdAt: new Date(),
+    };
+
+    await userRepository.save(user);
+
+    const savedUser = await userRepository.findById(user.id);
+
+    expect(savedUser).toEqual(user);
+  });
+
+  it('should update a user', async () => {
+    await userRepository.save(user);
+
+    const savedUser = (await userRepository.findById(user.id)) as User;
+
+    const userToUpdate: User = {
+      ...savedUser,
+      lastName: 'Travolta',
+    };
+
+    await userRepository.update(savedUser.id, userToUpdate);
+
+    const updatedUser = await userRepository.findById(savedUser.id);
+
+    expect(updatedUser?.lastName).toEqual('Travolta');
+    expect(updatedUser?.updatedAt).not.toBeNull();
+  });
+
+  it('should delete a user', async () => {
+    await userRepository.save(user);
+
+    const savedUser = await userRepository.findById(user.id);
+
+    expect(savedUser).not.toBeUndefined();
+
+    await userRepository.delete(user.id);
+
+    const deletedUser = await userRepository.findById(user.id);
+
+    expect(deletedUser).toBeUndefined();
+  });
+
+  it('should find all users', async () => {
+    const anotherUser: User = {
+      id: uuidv4(),
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      createdAt: new Date(),
+    };
+
+    await userRepository.save(user);
+    await userRepository.save(anotherUser);
+
+    const users = await userRepository.findAll();
+
+    expect(users.length).toEqual(2);
+  });
+});
